00b7d1d2aad0e0836894a6b5302aa8c2405222bf
Dinesh <dinabeysinghe@gmail.com>
2023-11-29 11:29:58 +0530

    created reducer funcion

---
+++ src/store/reducer.js
@@ -0,0 +1,27 @@
+function reducer(store, actions) {
+
+    switch (actions.type) {
+        case create_todo:
+            return; //update store
+        case update_todo:
+            return; //update store
+        case remove_todo:
+            return; //update store
+        case change_status_todo:
+            return; //update store
+
+        default:
+            reurn; //store
+    }
+
+
+    // if(action is create_todo){
+    //     todo.push(action.payload);
+    // } else if(action is update_todo){
+    //     finde and update todo
+    // } else if(action is remove_todo){
+    //     find and remove todo by action.payload
+    // } else if(action is change_status_todo){
+    //     find and update todo status by action.payload
+    // }
+}
