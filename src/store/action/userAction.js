a2c67255a24155d21c2c7c0bc719856e3d662be5
Dinesh <dinabeysinghe@gmail.com>
2023-12-03 11:06:46 +0530

    get API data for user

--- src/store/action/userAction.js
+++ src/store/action/userAction.js
@@ -1,15 +1,23 @@
+import axios from "axios";
+
 export const GET_USER_LIST = 'GET_USER_LIST';
 
-
 export function getUserList() {
-    return {
-        type: GET_USER_LIST,
-        payload: {
-            users: [
-                { name: 'Dinesh' },
-                { name: 'Abeysinghe' },
-                { name: 'Ravi' },
-            ]
-        }
-    }
+
+    axios.get('https://jsonplaceholder.typicode.com/users/')
+        .then(function (response) {
+            // handle success
+            console.log(response.data);
+            return {
+                type: GET_USER_LIST,
+                payload:
+                    response.data,
+            }
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+
+
 }
