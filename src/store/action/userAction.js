f1fb2a624d4482e910dcec6e3dc188064a009a73
Dinesh <dinabeysinghe@gmail.com>
2023-12-03 11:34:43 +0530

    data featching done using thunk middleware

--- src/store/action/userAction.js
+++ src/store/action/userAction.js
@@ -3,21 +3,13 @@ import axios from "axios";
 export const GET_USER_LIST = 'GET_USER_LIST';
 
 export function getUserList() {
-
-    axios.get('https://jsonplaceholder.typicode.com/users/')
-        .then(function (response) {
-            // handle success
-            console.log(response.data);
-            return {
+    return (dispatch) => {
+        axios.get('https://jsonplaceholder.typicode.com/users/').then((response) => {
+            dispatch({
                 type: GET_USER_LIST,
-                payload:
-                    response.data,
-            }
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-
+                payload: response.data,
+            });
+        });
+    }
 
 }
